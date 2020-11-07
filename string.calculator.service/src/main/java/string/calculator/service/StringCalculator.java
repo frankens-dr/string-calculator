fc812ad56243a4c29b36a7780e2a8f6c4be10ada
frankens-dr <imnikhilsrivastava@gmail.com>
2020-11-07 22:16:14 +0530

    final code refactoring done

--- string.calculator.service/src/main/java/string/calculator/service/StringCalculator.java
+++ string.calculator.service/src/main/java/string/calculator/service/StringCalculator.java
@@ -1,9 +1,9 @@
 package string.calculator.service;
 
 public class StringCalculator {
-	
-	static int calledCount;
-	
+
+	private int calledCount;
+
 	StringCalculator() {
 		calledCount = 0;
 	}
@@ -18,6 +18,8 @@ public class StringCalculator {
 		}
 		for (int i = 0; i < Number.length(); i++) {
 			x = Number.charAt(i);
+			if (i + 1 < Number.length())
+				x1 = Number.charAt(i + 1);
 			if (checkFordelimeters(x, x1)) {
 				if (x == '-') {
 					throw new RuntimeException("negatives not allowed");
@@ -35,13 +37,13 @@ public class StringCalculator {
 	}
 
 	public boolean checkFordelimeters(char x, char x1) {
-		if (x != '\n' && x != '/' && x != ';' && x != '\\' && x != 'n' && x1 != 'n' && x != ',' 
-				&& x != '*' && x != '[' && x != ']' && x != '%') {
+		if (x != '\n' && x != '/' && x != ';' && x != '\\' && x != 'n' && x1 != 'n' && x != ',' && x != '*' && x != '['
+				&& x != ']' && x != '%') {
 			return true;
 		}
 		return false;
 	}
-	
+
 	public int GetCalledCount() {
 		return calledCount;
 	}
